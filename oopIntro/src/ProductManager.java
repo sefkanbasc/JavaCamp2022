72e63a7b42272afbaf7ace8696610f51e5c31050
Sefkan Başçi <sefkanbasc@outlook.com>
2022-12-02 23:43:29 +0300

    JavaCamp2022
    OopIntroDay1

---
+++ oopIntro/src/ProductManager.java
@@ -0,0 +1,7 @@
+public class ProductManager {
+    // Method olarak kullandıgımız Class
+    public  void addToCart(Product product){
+        System.out.println("Sepete Eklendi = " + " " + product.name );
+
+    }
+}
